3d5a2d5fecf6f2ba997a8426c2e71408d3e98f50
Christopher <christopher.carnivale@gmail.com>
2019-04-12 19:21:03 -0400

    changed interval

--- src/server.js
+++ src/server.js
@@ -52,7 +52,7 @@ function startCatInterval(){
     else {
       clearInterval(catTimer);
     } 
-  }, 3000); 
+  }, 30000); 
 }
 
 function startDogInterval() { 
@@ -65,7 +65,7 @@ function startDogInterval() {
     else {
       clearInterval(dogTimer);
     } 
-  }, 3000); 
+  }, 30000); 
 }
 
 
